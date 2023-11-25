c25b039a04f27dc1235439fed675eb30f5a02467
david <kaneshiuchiha@gmail.com>
2023-11-25 09:34:01 -0500

    k commit

--- pages/images/[id]/index.jsx
+++ pages/images/[id]/index.jsx
@@ -1,17 +1,17 @@
-// import { useRouter } from 'next/router'
+import { useRouter } from 'next/router'
 
 const index = () => {
-    // const router = useRouter()
+    const router = useRouter()
 
     return (
         <>
             <div className='tj-container-img '>
                 <h1>WELCOME!</h1>
-                {/* <img src={router.query.id} alt="" /> */}
+                <img src={router.query.id} alt="" />
             </div>
-            {/* <div onClick={() => router.back()} className='fixed top-2 left-2 p-2 bg-white text-black rounded-md' >
+            <div onClick={() => router.back()} className='fixed top-2 left-2 p-2 bg-white text-black rounded-md' >
                 back
-            </div> */}
+            </div>
         </>
 
     )
